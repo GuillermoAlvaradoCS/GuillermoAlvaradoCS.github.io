8900319b1ff81d390943029d5fe7295c68c33263
GuilleAlvarado <guillermo.alvarado@jala-foundation.org>
2024-03-28 12:47:44 -0400

    main.jsx change

--- gemini_clone/src/main.jsx
+++ gemini_clone/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import ContextProvider from './Context/Context.jsx'
 import './index.css'
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root')).render(
   <ContextProvider>
     <App />
   </ContextProvider>,
 )
+
+reportWebVitals();
